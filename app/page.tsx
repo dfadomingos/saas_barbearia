18276afdf380024b396748a01ea8d20677c56f5a
dfadomingos <dfadomingos47@gmail.com>
2025-11-12 21:38:21 -0300

    feat: add barbershop item component

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,24 @@ import SearchInput from "./_components/search-input";
 import Image from "next/image";
 import banner from '../public/banner.png';
 import BookingItem from "./_components/booking-item";
+import { prisma } from "@/lib/prisma";
+import BarbershopItem from "./_components/barbershop-item";
 
-const Home = () => {
+const Home = async () => {
+  const recommendedBarbershops = await prisma.barbershop.findMany({
+    orderBy:{
+      name: "asc",
+    },
+  });
+  const popularBarbershops = await prisma.barbershop.findMany({
+    orderBy:{
+      name: "desc",
+    },
+  });
   return(
     <main>
       <Header />
-      <div className="px-5 space-y-4">
+      <div className="p-5 space-y-4">
         <SearchInput />
         <Image 
           src={banner} 
@@ -16,13 +28,33 @@ const Home = () => {
           sizes="100vw" 
           className="w-full h-auto"
         />
-        <h2 className="text-xs text-foreground font-semibold uppercase">Agendamentos</h2>
+        <h2 className="text-xs text-foreground font-semibold uppercase">
+          Agendamentos
+        </h2>
         <BookingItem 
           serviceName="Corte de cabelo"
           barbershopName="Barbearia do Joao"
           barbershopImageUrl="https://utfs.io/f/c97a2dc9-cf62-468b-a851-bfd2bdde775f-16p.png"
           date={new Date()}
         />
+        <h2 className="text-xs text-foreground font-semibold uppercase">
+          Recomendados
+        </h2>
+        <div className="flex gap-4 overflow-x-auto [&::-webkit-scrollbar]:hidden">
+          {recommendedBarbershops.map((barbershop) => (
+            <BarbershopItem key={barbershop.id} barbershop={barbershop} />          
+          ))}
+        </div>
+
+        <h2 className="text-xs text-foreground font-semibold uppercase">
+          Recomendados
+        </h2>
+        <div className="flex gap-4 overflow-x-auto [&::-webkit-scrollbar]:hidden">
+          {popularBarbershops.map((barbershop) => (
+            <BarbershopItem key={barbershop.id} barbershop={barbershop} />          
+          ))}
+        </div>
+        
       </div>      
     </main>
   );
