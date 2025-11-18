69cd2a9fb4059bdf3f39b864a3d9206b50e1a004
dfadomingos <dfadomingos47@gmail.com>
2025-11-18 17:00:53 -0300

    feat: add menu component

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -2,42 +2,36 @@
 
 import Image from "next/image"
 import { Button } from "./ui/button";
-import { LogInIcon, LogOutIcon, MenuIcon } from "lucide-react";
-import { authClient } from "@/lib/auth-client";
+import { MenuIcon } from "lucide-react";
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "./ui/sheet";
+import SidebarMenu from "./sidebar-menu";
 
-const Header = () => {
-    const {data: session} = authClient.useSession();
-    const handleLogin = async() => {
-        await authClient.signIn.social({
-            provider: 'google',
-        });
-    };
+const Header = () => {   
     return (
         <header className="flex itens-center justify-between px-5 py-6">
             <Image src="/logo.svg" alt="Aparatus" width={100} height={26.09}/>
-            <div className="flex items-center gap-2">
-                {session ? (
-                <div>
-                    <h1>{session.user?.name}</h1>
-                    <Button
-                    variant="outline"
-                    size="icon"
-                    onClick={() => authClient.signOut()}
-                    >
-                    <LogOutIcon />
-                    </Button>
-                </div>
-                ) : (
-                <Button variant="outline" size="icon" onClick={handleLogin}>
-                    <LogInIcon />
-                </Button>
-                )}
-                <Button variant="outline" size="icon">
-                <MenuIcon />
-                </Button>
+            <div className="flex items-center gap-2">                
+                <Sheet>
+                    <SheetTrigger asChild>
+                        <Button variant="outline" size="icon">
+                            <MenuIcon />
+                        </Button>
+                    </SheetTrigger>
+                    <SheetContent className="w-[370px] p-0">
+                        <SheetHeader className="border-b px-5 py-6 text-left">
+                            <SheetTitle className="text-lg font-bold">Menu</SheetTitle>
+                        </SheetHeader>
+                        <SidebarMenu />
+                    </SheetContent>
+                </Sheet>
             </div>                
-        </header>
-        
+        </header>        
     );    
 };
 export default Header;
